2616c284d676158b50364abcb3e45311569ac3fa
Andrew Yates <andrewmacu@me.com>
2019-09-20 21:33:42 -0400

    Successful database connection and starting files

---
+++ index.js
@@ -0,0 +1,119 @@
+const express = require('express'),
+    routescan = require('express-routescan'),
+    format = require('string-format'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    session = require("express-session"),
+    passport = require('passport'),
+    mongoose = require('mongoose'),
+    LocalStrategy = require("passport-local").Strategy;
+
+/**
+ * START GLOBALS
+ */
+global._base = __dirname + '/';
+
+console.info = function (message) {
+    console.log('[INFO] ' + message);
+};
+
+console.debug = function (message) {
+    if (_isDev) console.log('[DEBUG] ' + message);
+};
+
+console.critical = function (message) {
+    console.log('[!!! CRITICAL !!!] ' + message);
+};
+
+format.extend(String.prototype, {});
+String.prototype.replaceAll = function (a, b) {
+    return this.split(a).join(b);
+};
+
+/**
+ * START EXPRESS/DATABASE CONFIGURATION
+ */
+const app = express();
+
+global._env = app.get('env');
+global._isDev = _env === 'development';
+global._isProd = _env === 'production';
+
+console.info('Running in ' + _env + ' environment.');
+
+/**
+ * ESTABLISH DATABASE CONNECTION
+ */
+const url = 'mongodb+srv://admin687:admin687!@cluster0-au5yo.gcp.mongodb.net/test?retryWrites=true&w=majority';
+console.info('Attempting to connect to', url);
+mongoose.connect(url);
+mongoose.connection.on('connected', function () {
+    console.info('Database connection established');
+});
+
+mongoose.connection.on('error', function (err) {
+    console.critical('Cannot connect to database');
+    console.critical(JSON.stringify(err));
+    return process.exit();
+});
+
+mongoose.connection.on('disconnected', function () {
+    console.info('Database disconnected');
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(session({
+    secret: 'KRJFERKJGERG',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: false, maxAge: 10000000 }
+}));
+
+passport.serializeUser(function (user, done) {
+    done(null, user._id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+}, function (username, password, done) {
+    User.findOne({email: username}, function (err, user) {
+        if (err) {
+            return done(err);
+        }
+        if (!user) {
+            return done(null, false,
+                {message: "Invalid email or password."});
+        }
+        user.checkPassword(password, function (err, isMatch) {
+            if (err) {
+                return done(err);
+            }
+            if (isMatch) {
+                return done(null, user);
+            } else {
+                return done(null, false,
+                    {message: "Invalid email or password."});
+            }
+        });
+    });
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// app.use(baseResponse);
+routescan(app, {
+    ignoreInvalid: true
+});
+
+// app.use(errors.notFound, errors.handler);
+
+module.exports = app;
---
+++ models/user.js
@@ -0,0 +1,71 @@
+const bcrypt = require('bcrypt-nodejs');
+const mongoose = require('mongoose');
+const shortid = require('shortid');
+
+const SALT_FACTOR = 10;
+
+let userSchema = new mongoose.Schema({
+    _id: {type: String, default: shortid.generate},
+    fullName: {type: String, required: true},
+    password: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    cellPhoneNum: {type: String, required: true, unique: true},
+    userGroup: {type: [String], required: false}, /*ADMIN, DRIVER, CLIENT, etc...*/
+    created: {type: Date, required: true, default: Date.now}
+}, {collection: 'hestia_users'});
+
+let noop = function () {
+};
+
+userSchema.pre('save', function (done) {
+    let user = this; //Reference to user model
+
+    if (!user.isModified("password")) {
+        return done();
+    }
+
+    bcrypt.genSalt(SALT_FACTOR, function (err, salt) {
+        if (err) {
+            return done(err);
+        }
+        bcrypt.hash(user.password, salt, noop, function (err, hashedPassword) {
+            if (err) {
+                return done(err);
+            }
+            user.password = hashedPassword;
+            done();
+        });
+    });
+});
+
+userSchema.pre('findOneAndUpdate', function (done) {
+    let query = this; //Reference to user model
+    let user = query.getUpdate();
+
+    if (!user.password) {
+        return done();
+    }
+
+    bcrypt.genSalt(SALT_FACTOR, function (err, salt) {
+        if (err) {
+            return done(err);
+        }
+        bcrypt.hash(user.password, salt, noop, function (err, hashedPassword) {
+            if (err) {
+                return done(err);
+            }
+            user.password = hashedPassword;
+            done();
+        });
+    });
+});
+
+userSchema.methods.checkPassword = function (guess, done) {
+    bcrypt.compare(guess, this.password, function (err, isMatch) {
+        done(err, isMatch);
+    });
+};
+
+let user = mongoose.model('User', userSchema);
+
+module.exports = User;
